9d217cf6377161e18a58925de51977d48dea082a
natancolen <natancolen@gmail.com>
2023-08-04 17:51:56 -0300

    router enderecos criado

--- setup/urls.py
+++ setup/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
+from enderecos.api.viewsets import EnderecoViewSet
 from pontosturisticos.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracaoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracaoViewSet)
+router.register(r'enderecos', EnderecoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
