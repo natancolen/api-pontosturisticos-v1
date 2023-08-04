8755fc75ec69f716713e224c6674c59d828ebb55
natancolen <natancolen@gmail.com>
2023-08-04 17:47:32 -0300

    router atracao criado

--- setup/urls.py
+++ setup/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from rest_framework import routers
 
 from pontosturisticos.api.viewsets import PontoTuristicoViewSet
+from atracoes.api.viewsets import AtracaoViewSet
 
 router = routers.DefaultRouter()
 router.register(r'pontoturistico', PontoTuristicoViewSet)
+router.register(r'atracoes', AtracaoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
