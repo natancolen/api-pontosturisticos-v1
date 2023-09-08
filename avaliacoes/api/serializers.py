42dd3659a9c5cde38f198896889937762ad1bc99
natancolen <natancolen@gmail.com>
2023-09-08 16:25:09 -0300

    endoint avaliacoes criado

---
+++ avaliacoes/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework.serializers import ModelSerializer
+
+from avaliacoes.models import Avaliacao
+
+class AvaliacaoSerializer(ModelSerializer):
+
+    class Meta:
+        model = Avaliacao
+        field = '__all__'
--- setup/urls.py
+++ setup/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
+from avaliacoes.api.viewsets import AvaliacaoViewSet
 from comentarios.api.viewsets import ComentarioViewSet
 from enderecos.api.viewsets import EnderecoViewSet
 from pontosturisticos.api.viewsets import PontoTuristicoViewSet
@@ -12,6 +13,7 @@ router.register(r'pontoturistico', PontoTuristicoViewSet)
 router.register(r'atracoes', AtracaoViewSet)
 router.register(r'enderecos', EnderecoViewSet)
 router.register(r'comentarios', ComentarioViewSet)
+router.register(r'avaliacoes', AvaliacaoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
